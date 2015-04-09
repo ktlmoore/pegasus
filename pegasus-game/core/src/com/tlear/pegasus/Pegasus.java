789938f59a33bac842a0217517e28fd714a0588a
Tom Lear <tom@tlear.co.uk>
2015-04-09 22:13:30 +0100

    Added ship Sprite; displayed Ship.
    Preliminaries now exist in Pegasus.java for displaying the ship
    Model and texture initted
    Camera initted
    
    <div xmlns:cc="http://creativecommons.org/ns#" about="http://millionthvector.blogspot.co.uk/p/free-sprites.html"><a rel="cc:attributionURL" property="cc:attributionName" href="http://millionthvector.blogspot.com/p/free-sprites.html">MillionthVector</a> / <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a></div>
    
    for the sprite

--- pegasus-game/core/src/com/tlear/pegasus/Pegasus.java
+++ pegasus-game/core/src/com/tlear/pegasus/Pegasus.java
@@ -4,29 +4,62 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 
 public class Pegasus extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	
 	BitmapFont font;
+	
+	// Camera init
+	private OrthographicCamera camera;
+	
+	SpriteBatch batch;
+	
+	// Texture init
+	private Texture shipImage;
+	
+	// Model init
+	private Rectangle ship;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		
 		font = new BitmapFont();
 		font.setColor(Color.RED);
+		
+		// Initialise camera
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+		
+		// Initialise textures
+		shipImage = new Texture(Gdx.files.internal("ship.png"));
+		
+		// Initialise model
+		ship = new Rectangle();
+		ship.width = 95;
+		ship.height = 108;
+		
+		ship.x = 50;
+		ship.y = 50;
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
+		// Update the camera.
+		camera.update();
+		
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		font.draw(batch, "HELLO WORLD", 200, 200);
+		// Render the ship
+		batch.draw(shipImage, ship.x, ship.y);
 		batch.end();
 	}
 	
