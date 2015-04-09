a6c5c19a9fd1fb6e34779642b1507ca50184371e
Tom Lear <tom@tlear.co.uk>
2015-04-09 22:03:07 +0100

    Set up DesktopLauncher window with size and name (initial)

--- pegasus-game/desktop/src/com/tlear/pegasus/desktop/DesktopLauncher.java
+++ pegasus-game/desktop/src/com/tlear/pegasus/desktop/DesktopLauncher.java
@@ -7,6 +7,11 @@ import com.tlear.pegasus.Pegasus;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		
+		config.title = "Pegasus";
+		config.width = 800;
+		config.height = 480;
+		
 		new LwjglApplication(new Pegasus(), config);
 	}
 }
