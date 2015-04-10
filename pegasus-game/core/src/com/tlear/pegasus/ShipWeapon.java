88a390a4d429e58eed0184e979eb9699feecb2af
Tom Lear <tom@tlear.co.uk>
2015-04-10 22:57:34 +0100

    Created Ship Weapon interface

---
+++ pegasus-game/core/src/com/tlear/pegasus/ShipWeapon.java
@@ -0,0 +1,17 @@
+package com.tlear.pegasus;
+
+public interface ShipWeapon {
+	
+	public void setX();
+	public void setY();
+	public void setAngle();
+	
+	public void addX();
+	public void addY();
+	public void addAngle();
+	
+	public int getX();
+	public int getY();
+	public int getAngle();
+	
+}
